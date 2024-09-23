fa1eeaf339dfcbbfb5d78a256ffe935933ccbcfc
Blake Bradford <james.b.bradford@faa.gov>
2024-09-23 08:40:40 -0400

    update to be a union of values

--- models/column_set.py
+++ models/column_set.py
@@ -1,8 +1,16 @@
 from dataclasses import dataclass, field
 from typing import List
+from typing import List, Union
 
 
 @dataclass
 class ColumnSet:
-    all_columns: bool = False
-    columns: List[str] = field(default_factory=list)
+    columns: Union[List[str], bool, None] = field(default=None)
+
+    def __init__(self, columns: Union[List[str], bool, None] = None) -> None:
+        if columns == False:
+            raise ValueError("Columns must be a list of strings or a value of True")
+        elif not columns:
+            columns = None
+        else:
+            self.columns = columns
