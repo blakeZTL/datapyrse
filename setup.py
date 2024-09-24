6a75366dd09040b4420ec5a6bd93c4511bdcbc82
Blake Bradford <james.b.bradford@faa.gov>
2024-09-24 12:20:41 -0400

    add project setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+# Read the requirements from the requirements.txt file
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="datapyrse",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=requirements,
+)
